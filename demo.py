dcf44d8c06bc6fda764f80450827dbdc3fe08ea8
CodingMan <CodingMan@uptt.cc>
2020-09-17 09:39:04 +0800

    Adjust demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,15 @@
-
 from single_log.log import Logger
 
 if __name__ == '__main__':
+
+    logger = Logger('INFO', Logger.INFO)
+    logger.show_value(Logger.INFO, 'show int', 100)
+    logger.show_value(Logger.INFO, 'show str', 'this is a string')
+    logger.show_value(Logger.INFO, 'show int list', [101, 102, 103])
+    logger.show_value(Logger.INFO, 'show string list', ['101', '102', '103'])
+
+    print('=' * 20)
+
     logger = Logger('TRACE', Logger.TRACE)
 
     logger.show(Logger.INFO, 'It should be print')
@@ -33,3 +41,13 @@ if __name__ == '__main__':
     logger.show(Logger.TRACE, 'It should NOT be print')
 
     print('=' * 20)
+
+    def log_to_file(msg):
+        with open('single_log.txt', 'a', encoding='utf8') as f:
+            f.write(f'{msg}\n')
+
+    logger = Logger('INFO', Logger.INFO, handler=log_to_file)
+
+    logger.show(Logger.INFO, '1')
+    logger.show(Logger.INFO, 2)
+    logger.show_value(Logger.INFO, 'show value', 456)
