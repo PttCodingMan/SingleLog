7b4e3e47b77b9aad9b02ae74bbaecb00be6e43e0
CodingMan <CodingMan@uptt.cc>
2020-09-22 16:16:47 +0800

    Adjust setup

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='single_log\ngithub: https://github.com/PttCodingMan/py_single_logger',  # Required
+    description='single-log\ngithub: https://github.com/PttCodingMan/https://github.com/PttCodingMan/single-log',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
@@ -54,7 +54,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/PttCodingMan/py_single_logger',  # Optional
+    url='https://github.com/PttCodingMan/py_single-logger',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
