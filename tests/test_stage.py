3670261427077e7cd2ad3d5e57491d9a9d9b9a5a
PttCodingMan <pttcodingman@gmail.com>
2022-08-31 17:57:52 +0800

    Add test case.

--- tests/test_stage.py
+++ tests/test_stage.py
@@ -1,20 +1,21 @@
 import sys
 import time
 
+from colorama import Fore
+
 sys.path.insert(0, '../')
 
 from SingleLog import Logger
 
 
-def test_do():
+def test_stage():
     logger = Logger('rocket')
 
     logger.info('Init rocket launch proces')
-    time.sleep(1.5)
+    time.sleep(0.5)
     logger.stage('complete!')
 
     logger.info('Start the countdown')
-    time.sleep(1)
     logger.stage('3')
     time.sleep(1)
     logger.stage('2')
@@ -32,5 +33,13 @@ def test_do():
     logger.info("Don't worry, logger will be fine.")
 
 
+def test_stage_2():
+    logger = Logger('demo', stage_color_list=[Fore.GREEN, Fore.YELLOW])
+
+    logger.info('start')
+    for i in range(10):
+        logger.stage(i)
+
+
 if __name__ == '__main__':
-    test_do()
+    test_stage_2()
