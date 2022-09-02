17d4942bcf22e43af188a1b6bd201a7406584556
PttCodingMan <pttcodingman@gmail.com>
2022-09-02 17:09:03 +0800

    Add DeprecationWarning.

--- SingleLog/log.py
+++ SingleLog/log.py
@@ -1,3 +1,5 @@
+from warnings import warn
+
 from .SingleLog import Logger, LogLevel
 
 
@@ -7,3 +9,19 @@ class Logger(Logger):
     DEBUG = LogLevel.DEBUG
     INFO = LogLevel.INFO
     SILENT = LogLevel.SILENT
+
+    def __init__(self, *args, **kwargs):
+
+        warn(f'"from SingleLog.log import Logger" is deprecated, use "from SingleLog import Logger"',
+             DeprecationWarning, stacklevel=2)
+
+        handler = kwargs.pop('handler', None)
+        if handler is not None:
+            # DeprecationWarning('handler is deprecated, use callback instead')
+            callback = kwargs.pop('callback', None)
+            if callback is not None:
+                raise ValueError('callback and handler cannot be set at the same time')
+
+            kwargs['callback'] = handler
+
+        super().__init__(*args, **kwargs)
